4153f25e1d43427a6af92ee3be9a9183c0b04bd1
MHlushneu <maksim.hlushneu@gmail.com>
2019-01-23 16:33:35 +0300

    Refactoring general statistics JPQL query

--- src/main/java/com/vstu/department/repository/EmployeeParameterRepository.java
+++ src/main/java/com/vstu/department/repository/EmployeeParameterRepository.java
@@ -21,9 +21,9 @@ public interface EmployeeParameterRepository extends JpaRepository<EmployeeParam
                                                   @Param("tabel") String tabels);
 
     @Query("SELECT " +
-            "new com.vstu.department.dto.statistics.GeneralEmployeStatisticsDTO(ep.anketa.tabel, sum(ep.coefficient * ep.count)) " +
-            "FROM EmployeeParameter ep " +
-            "WHERE ep.anketa.halfYear IN :halfYears AND ep.anketa.tabel IN :tabels " +
-            "GROUP BY ep.anketa.tabel")
+            "new com.vstu.department.dto.statistics.GeneralEmployeStatisticsDTO(a.tabel, COALESCE(sum(ep.coefficient * ep.count),0)) " +
+            "FROM Anketa a LEFT JOIN a.parameters ep " +
+            "WHERE a.halfYear IN :halfYears AND a.tabel IN :tabels " +
+            "GROUP BY a.tabel")
     List<GeneralEmployeStatisticsDTO> getGeneralStatistics(@Param("halfYears") List<String> halfYears, @Param("tabels") List<String> tabels);
 }
