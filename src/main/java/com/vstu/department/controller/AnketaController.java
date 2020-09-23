4c40ef7bddc004c42914d3e56e3d2989b246009a
Admin <Admin@1k413s2>
2020-09-23 15:42:48 +0300

    Update AnketaController security

--- src/main/java/com/vstu/department/controller/AnketaController.java
+++ src/main/java/com/vstu/department/controller/AnketaController.java
@@ -1,18 +1,27 @@
 package com.vstu.department.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+import javax.validation.constraints.Size;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.vstu.department.dto.AnketaDTO;
 import com.vstu.department.dto.EmployeeDTO;
 import com.vstu.department.dto.EmployeeParameterDTO;
 import com.vstu.department.service.AnketaService;
 import com.vstu.department.util.UtilService;
 import com.vstu.department.util.ValidList;
-import lombok.RequiredArgsConstructor;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import javax.validation.constraints.Size;
-import java.util.List;
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -35,15 +44,16 @@ public class AnketaController {
     }
 
     @GetMapping("/employee/{tabel}")
-    @PreAuthorize("hasRole('DEP_HEAD')")
+    @PreAuthorize("hasAnyRole('DEP_HEAD', 'TEACHER')")
     public AnketaDTO getAnketaByTabel(@PathVariable String tabel,
-                                      @Size(max = 6, min = 6) @RequestParam String halfYear) {
+            @Size(max = 6, min = 6) @RequestParam String halfYear) {
         return anketaService.getByTabelAndHalfYear(tabel, halfYear);
     }
 
     @PutMapping("/{id}")
-    @PreAuthorize("hasRole('DEP_HEAD')")
-    public AnketaDTO updateAnketa(@PathVariable Long id, @Valid @RequestBody ValidList<EmployeeParameterDTO> parameters) {
+    @PreAuthorize("hasAnyRole('DEP_HEAD', 'TEACHER')")
+    public AnketaDTO updateAnketa(@PathVariable Long id,
+            @Valid @RequestBody ValidList<EmployeeParameterDTO> parameters) {
         return anketaService.updateAnketa(id, parameters);
     }
 }
