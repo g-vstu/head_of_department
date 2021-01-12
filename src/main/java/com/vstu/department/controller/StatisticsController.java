1f6b6b2e7ce3855e8ef5c4fd6cea6549f0496a15
Nifar <venera123555@gmail.com>
2021-01-12 13:03:35 +0300

    Fix StatisticsController.

--- src/main/java/com/vstu/department/controller/StatisticsController.java
+++ src/main/java/com/vstu/department/controller/StatisticsController.java
@@ -1,11 +1,7 @@
 package com.vstu.department.controller;
 
-import com.vstu.department.dto.statistics.GeneralEmployeStatisticsDTO;
-import com.vstu.department.dto.statistics.StatisticsDTO;
-import com.vstu.department.model.enums.ParameterGroupType;
-import com.vstu.department.service.StatisticsService;
-import com.vstu.department.util.UtilService;
-import lombok.RequiredArgsConstructor;
+import java.util.List;
+
 import org.apache.logging.log4j.util.Strings;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,7 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.vstu.department.dto.statistics.GeneralEmployeStatisticsDTO;
+import com.vstu.department.dto.statistics.StatisticsDTO;
+import com.vstu.department.model.enums.ParameterGroupType;
+import com.vstu.department.service.StatisticsService;
+import com.vstu.department.util.UtilService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,11 +26,12 @@ public class StatisticsController {
 
     @GetMapping
     @PreAuthorize("hasAnyRole('DEP_HEAD', 'VICE-RECTOR')")
-    public StatisticsDTO get(@RequestParam int type,
-                             @RequestParam String halfYear,
-                             @RequestParam(required = false) String department) {
+    public StatisticsDTO get(@RequestParam int type, @RequestParam String halfYear,
+            @RequestParam(required = false) String department) {
         String tabelHead = (String) UtilService.getFieldFromAuthentificationDetails("tabel");
-        if (tabelHead.startsWith(UtilService.getDefaultViceRectorPrefix()) && Strings.isNotEmpty(department) && department.length() > 3) {
+        if (tabelHead.startsWith(UtilService.getDefaultViceRectorPrefix())
+                && Strings.isNotEmpty(department) /** && department.length() > 3 */
+        ) {
             tabelHead = department;
         }
         return statisticsService.getEmployeeParameterStats(tabelHead, ParameterGroupType.getByIndex(type), halfYear);
@@ -37,7 +40,7 @@ public class StatisticsController {
     @GetMapping("/general")
     @PreAuthorize("hasAnyRole('DEP_HEAD', 'VICE-RECTOR')")
     public List<GeneralEmployeStatisticsDTO> getGeneralStatistics(@RequestParam String halfYear,
-                                                                  @RequestParam(required = false) String department) {
+            @RequestParam(required = false) String department) {
         String tabelHead = (String) UtilService.getFieldFromAuthentificationDetails("tabel");
         if (tabelHead.startsWith(UtilService.getDefaultViceRectorPrefix())) {
             tabelHead = Strings.isEmpty(department) ? UtilService.getDefaultViceRectorPrefix() : department;
