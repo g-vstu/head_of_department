2649a0f8453d0c649be515f471d0fa4a456e24bb
vstu vstu <vstu.deanery.backend@gmail.com>
2021-04-09 07:28:24 +0000

    adding the output of the parameters for calculating the amount and rounding

--- src/main/java/com/vstu/department/dto/statistics/GeneralEmployeStatisticsDTO.java
+++ src/main/java/com/vstu/department/dto/statistics/GeneralEmployeStatisticsDTO.java
@@ -1,13 +1,28 @@
 package com.vstu.department.dto.statistics;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
-@AllArgsConstructor
 public class GeneralEmployeStatisticsDTO {
 
     private String tabel;
 
     private Double sum;
+
+    private List<GeneralEmployeeStatisticsParamsDTO> params = new ArrayList<>();
+
+    public  GeneralEmployeStatisticsDTO (String tabel, Double sum){
+        this.tabel = tabel;
+        this.sum = (Math.ceil(sum * 100) / 100);
+    }
+
+    public GeneralEmployeStatisticsDTO (String tabel, Double sum, List<GeneralEmployeeStatisticsParamsDTO> params){
+        this.tabel = tabel;
+        this.sum = (Math.ceil(sum * 100) / 100);
+        this.params = params;
+    }
+
 }
