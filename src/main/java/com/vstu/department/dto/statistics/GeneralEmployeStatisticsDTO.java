b78074b6f1315d4087388270bc477d57c3279243
vstu vstu <vstu.deanery.backend@gmail.com>
2021-04-09 07:27:10 +0000

    GeneralEmployeStatisticsDTO.java edited online with Bitbucket

--- src/main/java/com/vstu/department/dto/statistics/GeneralEmployeStatisticsDTO.java
+++ src/main/java/com/vstu/department/dto/statistics/GeneralEmployeStatisticsDTO.java
@@ -1,28 +1,14 @@
 package com.vstu.department.dto.statistics;
-
+ 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
-public class GeneralEmployeStatisticsDTO {
+@AllArgsConstructor
+ public class GeneralEmployeStatisticsDTO {
+ 
+     private String tabel;
+ 
+     private Double sum;
 
-    private String tabel;
-
-    private Double sum;
-
-    private List<GeneralEmployeeStatisticsParamsDTO> params = new ArrayList<>();
-
-    public  GeneralEmployeStatisticsDTO (String tabel, Double sum){
-        this.tabel = tabel;
-        this.sum = (Math.ceil(sum * 100) / 100);
-    }
-
-    public GeneralEmployeStatisticsDTO (String tabel, Double sum, List<GeneralEmployeeStatisticsParamsDTO> params){
-        this.tabel = tabel;
-        this.sum = (Math.ceil(sum * 100) / 100);
-        this.params = params;
-    }
-
-}
+ }
